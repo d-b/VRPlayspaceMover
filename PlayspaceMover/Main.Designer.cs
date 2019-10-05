b1b31958ceb22a739eb62465a236592f748373ad
Daniel Bloemendal <d.bloemendal@gmail.com>
2019-10-05 17:04:27 -0400

    Added some rough initial poses

--- PlayspaceMover/Main.Designer.cs
+++ PlayspaceMover/Main.Designer.cs
@@ -31,11 +31,17 @@
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonLie = new System.Windows.Forms.Button();
+            this.buttonSit3 = new System.Windows.Forms.Button();
+            this.buttonSit2 = new System.Windows.Forms.Button();
+            this.buttonSit1 = new System.Windows.Forms.Button();
+            this.buttonTPose = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.logRichText = new System.Windows.Forms.RichTextBox();
             this.tabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,7 +54,7 @@
             this.tabControl.Location = new System.Drawing.Point(0, 0);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(766, 497);
+            this.tabControl.Size = new System.Drawing.Size(764, 493);
             this.tabControl.TabIndex = 0;
             // 
             // tabPage1
@@ -57,7 +63,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(758, 471);
+            this.tabPage1.Size = new System.Drawing.Size(756, 467);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Poses";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -70,22 +76,86 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonLie, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSit3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSit2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSit1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonTPose, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(752, 465);
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 461F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(750, 461);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonLie
+            // 
+            this.buttonLie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLie.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLie.Location = new System.Drawing.Point(603, 3);
+            this.buttonLie.Name = "buttonLie";
+            this.buttonLie.Size = new System.Drawing.Size(144, 455);
+            this.buttonLie.TabIndex = 4;
+            this.buttonLie.Text = "Lie";
+            this.buttonLie.UseVisualStyleBackColor = true;
+            this.buttonLie.Click += new System.EventHandler(this.buttonLie_Click);
+            // 
+            // buttonSit3
+            // 
+            this.buttonSit3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSit3.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSit3.Location = new System.Drawing.Point(453, 3);
+            this.buttonSit3.Name = "buttonSit3";
+            this.buttonSit3.Size = new System.Drawing.Size(144, 455);
+            this.buttonSit3.TabIndex = 3;
+            this.buttonSit3.Text = "Sit 3";
+            this.buttonSit3.UseVisualStyleBackColor = true;
+            this.buttonSit3.Click += new System.EventHandler(this.buttonSit3_Click);
+            // 
+            // buttonSit2
+            // 
+            this.buttonSit2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSit2.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSit2.Location = new System.Drawing.Point(303, 3);
+            this.buttonSit2.Name = "buttonSit2";
+            this.buttonSit2.Size = new System.Drawing.Size(144, 455);
+            this.buttonSit2.TabIndex = 2;
+            this.buttonSit2.Text = "Sit 2";
+            this.buttonSit2.UseVisualStyleBackColor = true;
+            this.buttonSit2.Click += new System.EventHandler(this.buttonSit2_Click);
+            // 
+            // buttonSit1
+            // 
+            this.buttonSit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSit1.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSit1.Location = new System.Drawing.Point(153, 3);
+            this.buttonSit1.Name = "buttonSit1";
+            this.buttonSit1.Size = new System.Drawing.Size(144, 455);
+            this.buttonSit1.TabIndex = 1;
+            this.buttonSit1.Text = "Sit 1";
+            this.buttonSit1.UseVisualStyleBackColor = true;
+            this.buttonSit1.Click += new System.EventHandler(this.buttonSit1_Click);
+            // 
+            // buttonTPose
+            // 
+            this.buttonTPose.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonTPose.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTPose.Location = new System.Drawing.Point(3, 3);
+            this.buttonTPose.Name = "buttonTPose";
+            this.buttonTPose.Size = new System.Drawing.Size(144, 455);
+            this.buttonTPose.TabIndex = 0;
+            this.buttonTPose.Text = "T-Pose";
+            this.buttonTPose.UseVisualStyleBackColor = true;
+            this.buttonTPose.Click += new System.EventHandler(this.buttonTPose_Click);
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(758, 471);
+            this.tabPage2.Size = new System.Drawing.Size(756, 467);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Settings";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -96,7 +166,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(758, 471);
+            this.tabPage3.Size = new System.Drawing.Size(756, 467);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Log";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -109,7 +179,7 @@
             this.logRichText.ForeColor = System.Drawing.Color.White;
             this.logRichText.Location = new System.Drawing.Point(3, 3);
             this.logRichText.Name = "logRichText";
-            this.logRichText.Size = new System.Drawing.Size(752, 465);
+            this.logRichText.Size = new System.Drawing.Size(750, 461);
             this.logRichText.TabIndex = 0;
             this.logRichText.Text = "";
             // 
@@ -117,13 +187,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 497);
+            this.ClientSize = new System.Drawing.Size(764, 493);
             this.Controls.Add(this.tabControl);
             this.Name = "Main";
             this.Text = "Playspace Mover";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
             this.Load += new System.EventHandler(this.Main_Load);
             this.tabControl.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -133,10 +205,15 @@
 
         private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.RichTextBox logRichText;
+        private System.Windows.Forms.Button buttonTPose;
+        private System.Windows.Forms.Button buttonLie;
+        private System.Windows.Forms.Button buttonSit3;
+        private System.Windows.Forms.Button buttonSit2;
+        private System.Windows.Forms.Button buttonSit1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- PlayspaceMover/Main.cs
+++ PlayspaceMover/Main.cs
@@ -11,8 +11,23 @@ using GlmSharp;
 
 namespace PlayspaceMover
 {
+    enum Pose
+    {
+        POSE_TPose,
+        POSE_Sit1,
+        POSE_Sit2,
+        POSE_Sit3,
+        POSE_Lie,
+    };
+
     public partial class Main : Form
     {
+        Library.LogDelegate logDelegate;
+        Library.UpdateDelegate updateDelegate;
+
+        Pose currentPose = Pose.POSE_TPose;
+        quat currentRotation;
+
         void Log(ulong level, string message)
         {
             logRichText.Invoke(new Action(() =>
@@ -47,21 +62,99 @@ namespace PlayspaceMover
 
         TrackerState Update(PlayState state)
         {
-            TrackerState trackers = new TrackerState();
-            float bodyHeight = 1.7f;
-            vec3 down = new vec3(0, -1, 0);
-            vec3 headRight = state.hmd.rot * new vec3(1, 0, 0); headRight.y = 0;
-            quat rotation = quatLookAtRH(headRight.Normalized, new vec3(0, 1, 0));
-            vec3 footRight = rotation * new vec3(0, 0, 1);
-            vec3 footForward = rotation * new vec3(-1, 0, 0);
+            if (currentRotation == null || currentPose == Pose.POSE_TPose || state.rotatePlayer != 0)
+            {
+                vec3 headRight = state.hmd.rot * new vec3(1, 0, 0); headRight.y = 0;
+                currentRotation = quatLookAtRH(headRight.Normalized, new vec3(0, 1, 0));
+            }
 
-            trackers.hip.pos = state.hmd.pos + down * (bodyHeight / 2.0f);
-            trackers.hip.rot = rotation;
-            trackers.lfoot.pos = state.hmd.pos + down * bodyHeight + (footForward - footRight) * 0.17f;
-            trackers.lfoot.rot = rotation;
-            trackers.rfoot.pos = state.hmd.pos + down * bodyHeight + (footForward + footRight) * 0.17f;
-            trackers.rfoot.rot = rotation;
-            return trackers;
+            float bodyHeight = 1.7f;
+
+            switch (currentPose)
+            {
+                case Pose.POSE_Sit1:
+                    {
+                        vec3 down = new vec3(0, -1, 0);
+                        vec3 footRight = currentRotation * new vec3(0, 0, 1);
+                        vec3 footForward = currentRotation * new vec3(-3.5F, 0, 0);
+
+                        TrackerState trackers = new TrackerState();
+                        trackers.hip.pos = state.hmd.pos + down * (bodyHeight / 2.0f);
+                        trackers.hip.rot = currentRotation;
+                        trackers.lfoot.pos = state.hmd.pos + down * bodyHeight * 0.75F + (footForward - footRight) * 0.17f;
+                        trackers.lfoot.rot = currentRotation;
+                        trackers.rfoot.pos = state.hmd.pos + down * bodyHeight * 0.75F + (footForward + footRight) * 0.17f;
+                        trackers.rfoot.rot = currentRotation;
+                        return trackers;
+                    }
+
+                case Pose.POSE_Sit2:
+                    {
+                        vec3 down = new vec3(0, -1, 0);
+                        vec3 footRight = currentRotation * new vec3(0, 0, 1);
+                        vec3 footForward = currentRotation * new vec3(-5, 0, 0);
+
+                        TrackerState trackers = new TrackerState();
+                        trackers.hip.pos = state.hmd.pos + down * (bodyHeight / 2.0f);
+                        trackers.hip.rot = currentRotation;
+                        trackers.lfoot.pos = state.hmd.pos + down * bodyHeight * 0.55f + (footForward - footRight) * 0.17f;
+                        trackers.lfoot.rot = currentRotation;
+                        trackers.rfoot.pos = state.hmd.pos + down * bodyHeight * 0.55f + (footForward + footRight) * 0.17f;
+                        trackers.rfoot.rot = currentRotation;
+                        return trackers;
+                    }
+
+                case Pose.POSE_Sit3:
+                    {
+                        vec3 down = new vec3(0, -1, 0);
+                        vec3 footRight = currentRotation * new vec3(0, 0, 1);
+                        vec3 footForward = currentRotation * new vec3(-10, 0, 0);
+
+                        TrackerState trackers = new TrackerState();
+                        trackers.hip.pos = state.hmd.pos + down * (bodyHeight / 2.0f);
+                        trackers.hip.rot = currentRotation;
+                        trackers.lfoot.pos = state.hmd.pos + down * bodyHeight * 0.45f + (footForward - footRight) * 0.17f;
+                        trackers.lfoot.rot = currentRotation * new quat(new vec3(0, 0, -1));
+                        trackers.rfoot.pos = state.hmd.pos + down * bodyHeight * 0.45f + (footForward + footRight) * 0.17f;
+                        trackers.rfoot.rot = currentRotation * new quat(new vec3(0, 0, -1));
+                        return trackers;
+                    }
+
+                case Pose.POSE_Lie:
+                    {
+                        vec3 headRight = state.hmd.rot * new vec3(0, 1, 0); headRight.x = 0; headRight.z = 0;
+                        quat currentLieRotation = quatLookAtRH(headRight.Normalized, new vec3(-1, 0, 0));
+
+                        vec3 down = currentRotation * new vec3(0, 0, -1);
+                        vec3 footRight = new vec3(0, -1, 0);
+                        vec3 footForward = currentRotation * new vec3(-1, 0, 0);
+
+                        TrackerState trackers = new TrackerState();
+                        trackers.hip.pos = state.hmd.pos + down * (bodyHeight / 2.0f);
+                        trackers.hip.rot = currentLieRotation;
+                        trackers.lfoot.pos = state.hmd.pos + down * bodyHeight + (footForward - footRight) * 0.17f;
+                        trackers.lfoot.rot = currentLieRotation;
+                        trackers.rfoot.pos = state.hmd.pos + down * bodyHeight + (footForward + footRight) * 0.17f;
+                        trackers.rfoot.rot = currentLieRotation;
+                        return trackers;
+                    }
+
+                default:
+                    {
+                        vec3 down = new vec3(0, -1, 0);
+                        vec3 footRight = currentRotation * new vec3(0, 0, 1);
+                        vec3 footForward = currentRotation * new vec3(-1, 0, 0);
+
+                        TrackerState trackers = new TrackerState();
+                        trackers.hip.pos = state.hmd.pos + down * (bodyHeight / 2.0f);
+                        trackers.hip.rot = currentRotation;
+                        trackers.lfoot.pos = state.hmd.pos + down * bodyHeight + (footForward - footRight) * 0.17f;
+                        trackers.lfoot.rot = currentRotation;
+                        trackers.rfoot.pos = state.hmd.pos + down * bodyHeight + (footForward + footRight) * 0.17f;
+                        trackers.rfoot.rot = currentRotation;
+                        return trackers;
+                    }
+            }
         }
 
         public Main()
@@ -76,7 +169,39 @@ namespace PlayspaceMover
             options.rightButtonMask = 1 << 7;
             options.resetButtonMask = 1 << 1;
             options.rotateButtonMask = 1 << 34 | 1 << 2;
-            Library.Init(Log, Update, options);
+            logDelegate = new Library.LogDelegate(Log);
+            updateDelegate = new Library.UpdateDelegate(Update);
+            Library.Init(logDelegate, updateDelegate, options);
+        }
+
+        private void buttonTPose_Click(object sender, EventArgs e)
+        {
+            currentPose = Pose.POSE_TPose;
+        }
+
+        private void buttonSit1_Click(object sender, EventArgs e)
+        {
+            currentPose = Pose.POSE_Sit1;
+        }
+
+        private void buttonSit2_Click(object sender, EventArgs e)
+        {
+            currentPose = Pose.POSE_Sit2;
+        }
+
+        private void buttonSit3_Click(object sender, EventArgs e)
+        {
+            currentPose = Pose.POSE_Sit3;
+        }
+
+        private void buttonLie_Click(object sender, EventArgs e)
+        {
+            currentPose = Pose.POSE_Lie;
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Library.Exit();
         }
     }
 }
